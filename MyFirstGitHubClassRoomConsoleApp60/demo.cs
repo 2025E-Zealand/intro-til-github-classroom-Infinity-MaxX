cc2c37b93f5fb4a41a3241206ebb8aba2fa9d6fd
Infinity-MaxX <katerina@oestergaard.com>
2025-08-27 11:10:19 +0200

    Added instances, adjusted the ID propety.

--- MyFirstGitHubClassRoomConsoleApp60/demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -8,16 +8,32 @@ namespace MyFirstGitHubClassRoomConsoleApp60
 {
     public class demo
     {
+        #region Instances
+        int _id = 0;
+        #endregion
         #region Properties
-        public int id {  get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
+        public int ID 
+        { 
+            get { return _id; } 
+            set { _id = value; }
+        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        #endregion
+
+        #region Constructor
+        public demo(string name, string description)
+        {
+            _id++;
+            Name = name;
+            Description = description;
+        }
         #endregion
 
         #region Methods
         public override string ToString()
         {
-            return $"ID: {id}\nName: {name}\nDescription: {description}";
+            return $"ID: {_id}\nName: {Name}\nDescription: {Description}";
         }
         #endregion
     }
