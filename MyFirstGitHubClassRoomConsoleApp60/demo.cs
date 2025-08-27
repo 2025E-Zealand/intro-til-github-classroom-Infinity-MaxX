4df361492dfa13a1300bb7cfe5c9ceae0fb26dfa
Infinity-MaxX <katerina@oestergaard.com>
2025-08-27 10:59:00 +0200

    Made simple properties.

--- MyFirstGitHubClassRoomConsoleApp60/demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/demo.cs
@@ -8,6 +8,10 @@ namespace MyFirstGitHubClassRoomConsoleApp60
 {
     public class demo
     {
-
+        #region Properties
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        #endregion
     }
 }
