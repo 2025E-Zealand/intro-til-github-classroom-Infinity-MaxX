4e314ebba62a9659fa2592a9f9c1e4d5f7f7693b
Infinity-MaxX <katerina@oestergaard.com>
2025-08-27 11:12:50 +0200

    Created an object of demo.

--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyFirstGitHubClassRoomConsoleApp60;
+
 Console.WriteLine("Hello, World!");
+
+demo d = new demo("Demo", "Dette er en demo.");
+Console.WriteLine(d);
